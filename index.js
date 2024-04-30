2a0382da09efc4168cbc3a501268e0bd6dec529e
Phu Vo <u1153426@umail.usq.edu.au>
2024-04-30 21:52:51 +1000

    Create and configure .env

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const route = require("./routes/client/index.route");
+const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
