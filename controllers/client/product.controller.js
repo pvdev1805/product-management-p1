1b0ed3cc91765a3261c757e9ac8cf1461d7b7f06
Phu Vo <u1153426@umail.usq.edu.au>
2024-05-01 14:54:02 +1000

    Create product list page(client)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,21 @@
+const Product = require("../../models/product.model");
+
 // [GET] /products/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  });
+
+  for (const item of products) {
+    item.priceNew = (
+      item.price -
+      (100 - item.discountPercentage) / 100
+    ).toFixed(2);
+  }
+
   res.render("client/pages/products/index", {
     pageTitle: "Product List",
+    products: products,
   });
 };
