88d9ff562472f40018f3e57c0aa50c728fb5d185
Phu Vo <u1153426@umail.usq.edu.au>
2024-05-02 13:08:58 +1000

    Implement feature: Filter-by-state Products (admin)

---
+++ public/admin/js/script.js
@@ -0,0 +1,21 @@
+// Button
+const listButtonStatus = document.querySelectorAll(`[button-status]`);
+
+if (listButtonStatus.length > 0) {
+  let url = new URL(window.location.href);
+
+  listButtonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+
+      window.location.href = url.href;
+    });
+  });
+}
+// Ends Button
