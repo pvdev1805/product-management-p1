5f9ea52a9d597748e8d5b8888baf451bffae943a
Phu Vo <u1153426@umail.usq.edu.au>
2024-05-02 23:05:41 +1000

    Refactor and Optimize feature: pagination (admin)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../models/product.model");
 const filterHelper = require("../../helpers/filter.helper");
+const paginationHelper = require("../../helpers/pagination.helper");
 
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
@@ -23,22 +24,9 @@ module.exports.index = async (req, res) => {
   // End - Feature: Search (Keyword)
 
   // Feature: Pagination
-  const objectPagination = {
-    currentPage: 1,
-    limitItems: 4,
-  };
-
-  if (req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  objectPagination.skip =
-    (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
   const countRecords = await Product.countDocuments();
-  objectPagination.totalPage = Math.ceil(
-    countRecords / objectPagination.limitItems
-  );
+
+  const objectPagination = paginationHelper(req, countRecords);
   // End - Feature: Pagination
 
   const products = await Product.find(find)
