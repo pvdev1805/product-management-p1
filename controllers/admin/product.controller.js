fdc95d68fa23a861837da878ee9c3225fa33d5ae
Phu Vo <u1153426@umail.usq.edu.au>
2024-05-03 16:07:33 +1000

    Implement Feature: Change Status - 1 Record (admin)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterHelper = require("../../helpers/filter.helper");
 const paginationHelper = require("../../helpers/pagination.helper");
+const systemConfig = require("../../config/system");
 
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
@@ -40,4 +41,21 @@ module.exports.index = async (req, res) => {
     keyword: req.query.keyword,
     objectPagination: objectPagination,
   });
+};
+
+// [GET] /admin/products/:status/:id
+module.exports.changeStatus = async (req, res) => {
+  const status = req.params.status;
+  const id = req.params.id;
+
+  await Product.updateOne(
+    {
+      _id: id,
+    },
+    {
+      status: status,
+    }
+  );
+
+  res.redirect("back");
 };
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,4 +5,6 @@ const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);
 
+router.get("/:status/:id", controller.changeStatus);
+
 module.exports = router;
