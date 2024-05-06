19db4db15798baa1922d59b0cf4c5d567f54573a
Phu Vo <u1153426@umail.usq.edu.au>
2024-05-06 21:52:39 +1000

    Implement feature: soft delete - 1 record (admin)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -90,9 +90,14 @@ module.exports.changeMulti = async (req, res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
 
-  await Product.deleteOne({
-    _id: id,
-  });
+  await Product.updateOne(
+    {
+      _id: id,
+    },
+    {
+      deleted: true,
+    }
+  );
 
   res.redirect("back");
 };
