89123df6e3b1b31edba2dfc13c2198d0c36e5af8
Phu Vo <u1153426@umail.usq.edu.au>
2024-05-14 13:27:10 +1000

    Feature: Create a new record (admin) - Add slug

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema(
   {
@@ -10,6 +12,11 @@ const productSchema = new mongoose.Schema(
     thumbnail: String,
     status: String,
     position: Number,
+    slug: {
+      type: String,
+      slug: "title",
+      unique: true,
+    },
     deleted: {
       type: Boolean,
       default: false,
